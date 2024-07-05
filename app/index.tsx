6c15f4890d6c04c661b5f2f19e123dc0f83e51c8
Sanio <krasulazr@gmail.com>
2024-07-05 13:36:57 +0300

    add nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,14 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Link } from 'expo-router';
+import React from 'react';
 
 export default function HomeScreen() {
   return (
-   <View style={styles.container}>
-    <Text>Auro!</Text>
+   <View className="flex-1 items-center justify-center bg-white">
+    <Text className="text-3xl font-pbold">Auro!</Text>
     <StatusBar style="auto"/>
-    <Link href="./profile">Go to profile</Link>
+    <Link className='text-[25px]' href="./profile">Go to profile</Link>
    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
